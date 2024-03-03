738f188bffcd0490d040f0697e6469c20a56d412
NotRealHumanBean <87423645+NotRealHumanBean@users.noreply.github.com>
2024-03-03 17:22:13 +0200

    initial roundrock

---
+++ js/form.js
@@ -0,0 +1,74 @@
+"use strict"
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        let formdata = new FormData(form);
+        
+
+        if (error === 0) {
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+                form.classList.remove('_sending');
+            } else {
+                alert('Something went wrong monseniour');
+                form.classList.remove('_sending');
+            }
+        } else {
+            alert('Some of your required fields are empty!')
+        }
+    }
+
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+        console.log(formReq);
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            console.log(input);
+            formRemoveError(input);
+            console.log("___________________________________________________ ")
+            console.log(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                    console.log("I AM HERE FIRST IF");
+                } 
+                } else {
+                    console.log("I AM HERE");
+                    if (input.value === '') {
+                        formAddError(input);
+                        error++;
+                    }
+            }
+        }
+        return error;
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
